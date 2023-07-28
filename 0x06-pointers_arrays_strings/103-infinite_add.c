56c7fc866876a1848c8b602455d38f7b76c12d15
CarsonWisdom <amoswwmayoka@gmail.com>
2023-07-28 06:26:27 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,3 +1,4 @@
+int overflow = 0, i = 0, j = 0, digits = 0;
 int val1 = 0, val2 = 0, temp_tot = 0;
 
 while (*(n1 + i) != '\0')
@@ -13,7 +14,7 @@ if (j >= size_r || i >= size_r)
 	if (i < 0)
 		val1 = 0;
 	else
-		val1 = '(n1 + i) - '0';
+		val1 = *(n1 + i) - '0';
 	if (j < 0)
 		val2 = 0;
 	else
@@ -23,16 +24,17 @@ if (j >= size_r || i >= size_r)
 		overflow = 1;
 	else
 		overflow = 0;
-	if (digits >= (size_r - 10)
-			return (0);
-			*(r + digits) = (temp_tot % 10) + '0';
-			digits++;
-			j--;
-			i--;
-			}
-			if (digits == size_r)
-			return (0);
-			*(r + digits) = '\0';
-			rev_string(r);
-			return (r);
+	if (digits >= (size_r - 1))
+		return (0);
+	*(r + digits) = (temp_tot % 10) + '0';
+	digits++;
+	j--;
+	i--;
+}
+if (digits == size_r)
+	return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
+	}
 
