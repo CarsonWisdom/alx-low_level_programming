fea57f953a5011098a930ce58358aa0e360b59bb
CarsonWisdom <amoswwmayoka@gmail.com>
2023-10-19 15:32:33 -0700

    hack

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. h
-export LD_PRELOAD="$PWD/../libgiga.so"
+wget -P .. https://raw.githubusercontent.com/CarsonWisdom/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"
